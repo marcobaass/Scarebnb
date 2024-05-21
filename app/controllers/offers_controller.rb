b6a09fdbee510babbe9055f81c5e38f6ae5203c4
Roberto Piazzolla <sertopico@gmail.com>
2024-05-21 15:50:59 +0200

    finish backend that allows creating offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,9 +4,11 @@ class OffersController < ApplicationController
   end
 
   def create
-    @offer = Offer.new(params[:offer])
+    @offer = Offer.new(offer_params)
+    # @offer.user_id = current_user.id
+    @offer.user = current_user
     if @offer.save
-    redirect_to_offer_path(@offer)
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
