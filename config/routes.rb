0f2d565d12dd42e1a335e8772efbdfab1adcb00c
Marco Baass <picture@marcobaass.com>
2024-05-21 17:08:54 +0200

    dashboardnew

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root to: 'offers#index'
   devise_for :users
-  get 'offers/index', to: 'offers#index', as: :offers
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -11,6 +9,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :offers, only: [:new, :create]
+  resources :offers, only: [:new, :index, :create]
   get 'dashboard', to: 'pages#dashboard'
 end
