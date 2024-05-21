cbb9834be97b3e88086be8eae8bc92cea2be23ba
Jeremy Rome <jeremy.rome93@gmail.com>
2024-05-21 15:03:26 +0200

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Destroying all user entries, offers and bookings"
+Booking.destroy_all
+Offer.destroy_all
+User.destroy_all
+
+puts "Creating users"
+User.create!(email: 'jeremy.rome93@gmail.com', password: '123456')
+User.create!(email: 'clemence.lc@gmail.com', password: '123456')
+User.create!(email: 'picture@marcobaass.com', password: '123456')
+user = User.new(email: 'sertopico@gmail.com', password: '123456')
+user.save!
+puts 'Users created'
+
+puts 'Creating new offers'
+offer = Offer.new(title: "Marco's haunted house", images: "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2023/12/image29-e1701415101357.jpg", description: "Boo-hoo", price: 899.99, city: 'Berlin', address: 'Rudi Dutschi', user_id: user.id)
+offer.save!
+
+puts 'Creating bookings'
+booking = Booking.new(status: 1, start_date: '2024/10/11', end_date: '2024/10/15', user_id: user.id, offer_id: offer.id)
+booking.save!
